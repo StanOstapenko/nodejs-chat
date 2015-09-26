cee7a16b33fa23a0e9c40d28df7352718c266ffc
Stan <stasmts@gmail.com>
2015-09-27 01:27:42 +0300

    Long polling implementation, template update

---
+++ script.js
@@ -0,0 +1,44 @@
+/**
+ * Created by stasostapenko on 9/27/15.
+ */
+
+publish.onsubmit = function(){
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('POST', '/publish', true);
+
+	xhr.send(JSON.stringify({
+		message: this.elements.message.value
+	}));
+
+	this.elements.message.value = '';
+
+	return false;
+};
+
+//Long polling implementation
+subscribe();
+
+function subscribe() {
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('GET', '/subscribe', true);
+
+	xhr.onload = function() {
+		var tr = document.createElement('tr'),
+				td = document.creaateElement('td');
+
+		td.textContent = this.response;
+		tr.appendChild(td);
+
+		messages.appendChild(tr);
+
+		subscribe();
+	};
+
+	xhr.send('');
+
+
+}
+
+
